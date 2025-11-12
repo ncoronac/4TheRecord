99b369b11317306e292d647e8652f9dc5a2849e2
Noemi Corona Calvario <coronanoemi177@gmail.com>
2025-11-12 12:37:22 -0800

    mood trackers not connected to backend tho

---
+++ packages/react-frontend/src/Icons.jsx
@@ -0,0 +1,17 @@
+//import React, { useState } from 'react';
+import './Icons.css';
+
+function Icons ({ text, num }) {
+    function handleClick(event) {
+        event.preventDefault();
+        console.log("Submitted: ", text, num);
+    }
+
+    return (
+        <button className="icon-button" onClick={handleClick}>
+            {text}
+        </button>
+    );
+}
+
+export default Icons;
---
+++ packages/react-frontend/src/TrackerContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Icons from "./Icons";
+import "./Icons.css";
+
+const TrackerContainer = ({ title, icons }) => {
+    return (
+        <div className="tracker-container">
+            <h3>{title}</h3>
+            <div className="icon-row">
+                {icons.map((icon, index) => (
+                    <Icons key={index} text={icon.text} num={icon.num}/>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default TrackerContainer;
