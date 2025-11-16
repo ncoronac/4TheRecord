8f21558fb350b7366f10efdc372fb1a7c9ca220b
Ava Romano <avaromano@Avas-MacBook-Pro-10.local>
2025-11-15 20:45:50 -0800

    dummy change for testing

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -2,6 +2,8 @@ import ReactDOMClient from "react-dom/client";
 import App from "./App.jsx";
 import "./main.css";
 
+// test test
+
 const container = document.getElementById("root");
 
 // Create a root
