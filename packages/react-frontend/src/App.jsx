bd6e81ee4683cfca7b179d6414824ff47eb556b6
Ava Romano <ajromano@calpoly.edu>
2025-12-04 18:14:56 -0800

    azure debugging

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -152,6 +152,7 @@ function App() {
         return promise;
     }
     function loginUser(creds) {
+        console.log("fetch failed");
         const promise = fetch(
             `https://4therecord-dycbdgaxc8cvdpb3.westus-01.azurewebsites.net/login`,
             {
